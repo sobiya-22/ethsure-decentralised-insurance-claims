fffca99c4fca5f503a16c5206ae104ccff85ce7e
sobiya-22 <sobiyashaikh22@gmail.com>
2025-07-26 23:20:47 +0530

    lokup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-// import axios from "axios";
+import axios from "axios";
 
 function App() {
-  // const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:3000/") 
-  //     .then((res) => {
-  //       setMessage(res.data);
-  //     })
+  useEffect(() => {
+    axios.get("http://localhost:3000/") 
+      .then((res) => {
+        setMessage(res.data);
+      })
 
-  // }, []);
+  }, []);
 
   return (
     <>
       <h1 className="text-3xl font-bold text-green-500">
-      Hello world!
+        Hello world! {message ? message : "Loading..."}
     </h1>
     </>
   );
