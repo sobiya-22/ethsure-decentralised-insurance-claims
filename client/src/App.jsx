6e93eab78d50d715906d6f81d348ec5924478e2f
sobiya-22 <sobiyashaikh22@gmail.com>
2025-07-26 23:17:29 +0530

    tailwind conifg added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,23 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import axios from "axios";
+// import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("");
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/") 
-      .then((res) => {
-        setMessage(res.data);
-      })
+  // useEffect(() => {
+  //   axios.get("http://localhost:3000/") 
+  //     .then((res) => {
+  //       setMessage(res.data);
+  //     })
 
-  }, []);
+  // }, []);
 
   return (
     <>
-      <h1>EthSure</h1>
-      <div className="card">
-        <h1>{message}</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold text-green-500">
+      Hello world!
+    </h1>
     </>
   );
 }
