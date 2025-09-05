ad5812d548eca097aca285d06cb471a9a095adc4
SakshiNehe <sakshinehe22@gmail.com>
2025-09-05 18:45:42 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
-import connectDB from "./db/connectDB.js";
+import connectDB from "./utils/connectDB.js";
 dotenv.config();
 
 const app = express();
